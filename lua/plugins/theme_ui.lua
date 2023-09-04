056a3f8e8af2af4f7556ac4381cd7bbdc1e7f6a3
Thorhian <thorhian@gmail.com>
2023-09-04 10:55:27 -0600

    More Migration Work

--- lua/plugins/theme_ui.lua
+++ lua/plugins/theme_ui.lua
@@ -1,3 +1,37 @@
+local lualine_table = {
+   options = {
+      globalstatus = true,
+   },
+   sections = {
+      lualine_a = {
+         {
+            'mode',
+            icons_enabled = true,
+            fmt = function (_, _)
+               return "󰂫"
+            end,
+         }
+      },
+      lualine_b = {'branch', 'diff', 'diagnostics'},
+      lualine_c = {'filename'},
+      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'}
+   },
+   inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {'filename'},
+      lualine_x = {'location'},
+      lualine_y = {},
+      lualine_z = {}
+   },
+   tabline = {},
+   winbar = {
+      --lualine_z = {'filename'},
+   },
+}
+
 return {
    -- Theming & UI Looks
    {
@@ -15,7 +49,33 @@ return {
       priority = 1000,
    },
 
-   { "rcarriga/nvim-notify" },
-   { 'kyazdani42/nvim-web-devicons' },
-   { 'lewis6991/gitsigns.nvim' },
+   {
+      "nvim-lualine/lualine.nvim",
+      config = function()
+         require("lualine").setup(lualine_table)
+      end
+   },
+
+   {
+      "rcarriga/nvim-notify",
+      config = function()
+         require("notify").setup()
+      end
+   },
+
+   {
+      "lewis6991/gitsigns.nvim",
+      config = function()
+         require("gitsigns").setup()
+      end
+   },
+
+   {
+      "goolord/alpha-nvim",
+      config = function ()
+         require("alpha").setup(require("alpha.themes.dashboard").config)
+      end
+   },
+
+   { "kyazdani42/nvim-web-devicons" },
 }
