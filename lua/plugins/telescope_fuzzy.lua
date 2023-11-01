b236f8d63db5900f9a5d644280debf97fdf49153
Thorhian <thorhian@protonmail.com>
2023-11-01 16:16:16 -0400

    Changed fzf to compile with cmake

--- lua/plugins/telescope_fuzzy.lua
+++ lua/plugins/telescope_fuzzy.lua
@@ -32,7 +32,10 @@ return {
    {
       "nvim-telescope/telescope.nvim", version = "0.1.2",
       dependencies = {
-         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+         { 
+            "nvim-telescope/telescope-fzf-native.nvim", 
+            build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build"
+         },
          { "nvim-telescope/telescope-symbols.nvim" },
          {
             "ahmedkhalf/project.nvim",
