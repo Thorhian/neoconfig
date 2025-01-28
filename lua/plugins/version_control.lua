73aebbff0ac811ec00fdff2f7f4fa8c5c6af6ee4
Thorhian <thorhian@protonmail.com>
2025-01-28 09:32:30 -0500

    Add neogit for testing

--- lua/plugins/version_control.lua
+++ lua/plugins/version_control.lua
@@ -25,5 +25,17 @@ return {
       config = gitsigns_config
    },
 
-   { "sindrets/diffview.nvim" }
+   { "sindrets/diffview.nvim" },
+
+   {
+      "NeogitOrg/neogit",
+      dependencies = {
+         "nvim-lua/plenary.nvim",         -- required
+         "sindrets/diffview.nvim",        -- optional - Diff integration
+
+         -- Only one of these is needed, not both.
+         "nvim-telescope/telescope.nvim", -- optional
+      },
+      config = true
+   }
 }
